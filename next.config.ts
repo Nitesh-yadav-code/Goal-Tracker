b420099a5f8cf4fc83e99f94b06710ac1e3c77d0
Nitesh yadav <kuamrniteshraj44@gmail.com>
2025-05-02 20:36:26 +0545

    image issue fixed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'randomuser.me'], // Add allowed domains here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+    ],
   },
 };
 
